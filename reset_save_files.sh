dc189f42df3abf0839b4ad1ae46e0f161375b9f6
SemkiShow <mgdeveloper123@gmail.com>
2025-07-11 13:35:05 +0200

    Remove unnecessary stuff from reset_save_files.sh

--- reset_save_files.sh
+++ reset_save_files.sh
@@ -5,15 +5,6 @@ set -e
 if [ "$1" == "" ]; then
     rm settings.txt
 fi
-if [ ! -f version.txt ]; then
-    echo "" > version.txt
-fi
-if [ "$1" == "" ]; then
-    rm -r timetables/
-fi
-if [ "$1" == "" ]; then
-    rm -r templates/
-fi
 if [ ! -d timetables ]; then
     mkdir timetables
 fi
